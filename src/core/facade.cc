61d2ca18edfa496095954d8e1fd32edd8142a764
vf <vf>
2025-11-04 00:05:53 +0100

    how to pass a dynamic method as a callback?

--- src/core/facade.cc
+++ src/core/facade.cc
@@ -11,9 +11,11 @@ void Facade::createCube() {
 }
 
 Napi::Object Facade::createExports(Napi::Object exports) {
+    std::function<void(Napi::CallbackInfo info)> createCube = std::bind(&Facade::createCube, this);
+
     exports.Set(
         Napi::String::New(env, "createCube"),
-        Napi::Function::New(env, Facade::createCube)
+        Napi::Function::New(env, createCube)
     );
 
     return exports;
